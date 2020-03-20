126447cbf50d8d6ac53af73cff54fb5eab002a98
Ahsan-khan <ahsan.k@live.com>
2020-03-20 14:21:26 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Initialize the variables
+INPUT_FILE=$1
+OUTPUT_FILE=$2
+CURRENT_DIR=$(dirname $0)
+
+# Call the python script
+echo "Calling process_federal_complaints.py script with provided args"
+${CURRENT_DIR}/src/consumer_complaints.py ${INPUT_FILE} ${OUTPUT_FILE}
